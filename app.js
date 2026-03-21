d517e797866c80041ef93334e5cad823e794164b
Aleksandr Visotskiy <sasavisik@gmail.com>
2026-03-21 23:59:14 +0300

    Продвинутые типы сужение Union

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 /* ========================Продвинутые типы =============================== */
 Object.defineProperty(exports, "__esModule", { value: true });
 // ========================Union==================
+// function logId(id: string | number | boolean) {
+// 	console.log(id);
+// }
+// logId(1);
+// logId("Hello");
+// logId(true);
 function logId(id) {
-    console.log(id);
+    if (typeof id === "string") {
+        console.log(id);
+    }
+    else {
+        console.log(id);
+    }
 }
-logId(1);
-logId("Hello");
-logId(true);
--- app.ts
+++ app.ts
@@ -2,9 +2,40 @@
 
 // ========================Union==================
 
+// function logId(id: string | number | boolean) {
+// 	console.log(id);
+// }
+// logId(1);
+// logId("Hello");
+// logId(true);
 function logId(id: string | number | boolean) {
-	console.log(id);
+	if (typeof id === "string") {
+		console.log(id); // id: string
+	} else {
+		console.log(id); // id: number | boolean
+	}
 }
-logId(1);
-logId("Hello");
-logId(true);
+
+function logError(err: string | string[]) {
+	if (Array.isArray(err)) {
+	console.log(err); // err: srting[];
+	}else {
+		console.log(err);
+	}
+}
+
+function logObject(obj: {a: number} | {b: number}) {
+	if ("a" in obj) {
+	console.log(obj.a);
+	}else {
+		console.log(obj.b);
+	}
+}
+
+function logMultipleIds(a: string | number, b: string | boolean) {
+	if (a === b) {
+		
+	} else {
+		console.log(a)
+	}
+}
